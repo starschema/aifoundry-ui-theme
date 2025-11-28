d0512623259343f85599d61415fbcfea2842bca0
Formanek Dénes <formdenes@gmail.com>
2025-11-28 14:43:08 +0100

    Latest build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,43 +1,130 @@
-import * as _mui_material_styles from '@mui/material/styles';
+import { Theme } from '@mui/material';
 
-declare const hclTheme: _mui_material_styles.Theme;
+// import { PaginationItemProps } from "@mui/material/PaginationItem";
+
+declare const hclTheme: Theme;
+
+declare module "@mui/material/InputBase" {
+  interface InputBasePropsSizeOverrides {
+    large: true;
+  }
+}
 
 declare module "@mui/material/ToggleButtonGroup" {
-    interface ToggleButtonGroupPropsSizeOverrides {
-        mini: true;
-    }
+  interface ToggleButtonGroupPropsSizeOverrides {
+    mini: true;
+  }
 }
+
+declare module "@mui/material/ToggleButtonGroup" {
+  interface ToggleButtonGroupPropsSizeOverrides {
+    mini: true;
+  }
+}
+
 declare module "@mui/material/Button" {
-    interface ButtonPropsSizeOverrides {
-        mini: true;
-    }
-    interface ButtonPropsVariantOverrides {
-        ghost: true;
-        destructive: true;
-        hcl: true;
-        ai: true;
-    }
+  interface ButtonPropsSizeOverrides {
+    mini: true;
+  }
+
+  interface ButtonPropsVariantOverrides {
+    ghost: true;
+    destructive: true;
+    hcl: true;
+    ai: true;
+  }
 }
+
+declare module "@mui/material/Button" {
+  interface ButtonPropsSizeOverrides {
+    mini: true;
+  }
+
+  interface ButtonPropsVariantOverrides {
+    ghost: true;
+    destructive: true;
+    hcl: true;
+    ai: true;
+  }
+}
+
 declare module "@mui/material/Alert" {
-    interface AlertPropsVariantOverrides {
-        iconFlipped: true;
-    }
+  interface AlertPropsVariantOverrides {
+    iconFlipped: true;
+  }
 }
+
+declare module "@mui/material/Alert" {
+  interface AlertPropsVariantOverrides {
+    iconFlipped: true;
+  }
+}
+
 declare module "@mui/material/IconButton" {
-    interface IconButtonPropsSizeOverrides {
-        mini: true;
-    }
-    interface IconButtonPropsVariantOverrides {
-        ghost: true;
-        destructive: true;
-    }
+  interface IconButtonPropsSizeOverrides {
+    mini: true;
+  }
+
+  interface IconButtonPropsVariantOverrides {
+    ghost: true;
+    destructive: true;
+  }
 }
+
+declare module "@mui/material/IconButton" {
+  interface IconButtonPropsSizeOverrides {
+    mini: true;
+  }
+
+  interface IconButtonPropsVariantOverrides {
+    ghost: true;
+    destructive: true;
+  }
+}
+
 declare module "@mui/material/PaginationItem" {
-    interface PaginationItemOwnProps {
-        disableFocusRipple?: boolean;
-        disableRipple?: boolean;
-        disableTouchRipple?: boolean;
-    }
+  interface PaginationItemOwnProps {
+    disableFocusRipple?: boolean;
+    disableRipple?: boolean;
+    disableTouchRipple?: boolean;
+  }
+}
+
+// declare module "@mui/material/PaginationItem" {
+//   interface PaginationItemTypeMap<
+//     AdditionalProps = {disableFocusRipple?: boolean;
+//     disableRipple?: boolean;
+//     disableTouchRipple?: boolean;},
+//     RootComponent extends React.ElementType = "div",
+//   > {
+//     props: AdditionalProps & PaginationItemOwnProps;
+//     defaultComponent: RootComponent;
+//   }
+// }
+// declare module "@mui/material/PaginationItem" {
+//   type PaginationItemProps = {
+//     disableFocusRipple?: boolean;
+//     disableRipple?: boolean;
+//     disableTouchRipple?: boolean;
+//   };
+// }
+
+declare module "@mui/material/Badge" {
+  interface BadgePropsVariantOverrides {
+    contained: true;
+    outlined: true;
+    ghost: true;
+    destructive: true;
+  }
+}
+
+declare module "@mui/material/Badge" {
+  interface BadgePropsVariantOverrides {
+    contained: true;
+    outlined: true;
+    ghost: true;
+    destructive: true;
+  }
 }
 
 export { hclTheme };
